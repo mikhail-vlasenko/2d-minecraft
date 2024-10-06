c2fbde1dc3a491db1683d66a197758f07c6e7ff8
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2024-10-07 00:15:14 +0200

    absorb muzero-general that was adapted and tested on cartpole

---
+++ reinforcement_learning/muzero-general/test.py
@@ -0,0 +1,25 @@
+import torch
+
+
+def mlp(
+    input_size,
+    layer_sizes,
+    output_size,
+    output_activation=torch.nn.Identity,
+    activation=torch.nn.ELU,
+):
+    sizes = [input_size] + layer_sizes + [output_size]
+    layers = []
+    for i in range(len(sizes) - 1):
+        act = activation if i < len(sizes) - 2 else output_activation
+        layers += [torch.nn.Linear(sizes[i], sizes[i + 1]), act()]
+    return torch.nn.Sequential(*layers)
+
+
+torch.nn.DataParallel(
+        mlp(
+            16,
+            [6, 2],
+            5,
+        ),
+)
